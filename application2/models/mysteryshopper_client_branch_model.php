60dc9bb27ca8a77444b592035c47a56610908f09
Sohail <asohail4@trgworld.com>
2018-08-09 13:48:49 +0500

    file name fix

--- application2/models/mysteryshopper_client_branch_model.php
+++ application2/models/mysteryshopper_client_branch_model.php
@@ -2,7 +2,10 @@
 
 Class Mysteryshopper_client_branch_model extends CI_Model {
 
-
+	function __construct()
+	{
+		parent::__construct();
+	}
 	function get_all()
 	{
 		//$sql = $this->db->query("Select * from nww_client");
